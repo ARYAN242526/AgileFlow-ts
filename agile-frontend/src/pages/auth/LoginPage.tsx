a8efae16882c9164d19f9b0909a6ab8acacc4359
ARYAN242526 <roy2503aryan@gmail.com>
2026-03-19 18:23:30 +0530

    feat : added login Page

--- agile-frontend/src/pages/auth/LoginPage.tsx
+++ agile-frontend/src/pages/auth/LoginPage.tsx
@@ -1,7 +1,36 @@
+import { useState } from "react";
+import { loginUser } from "../../services/authService";
 
 function LoginPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const res = await loginUser({ email, password });
+      console.log(res);
+    } catch (error) {
+      console.error(error);
+    };
+  } 
   return (
-    <div>LoginPage</div>
+    <div>
+      <h2>Login</h2>
+
+      <input
+      type="email"
+      placeholder="Email"
+      onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <input
+      type="password"
+      placeholder="Password"
+      onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button onClick={handleLogin}>Login</button>
+    </div>
   )
 }
 
